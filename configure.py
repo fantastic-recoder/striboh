fb4bc2e18b89fb8bc3a67bc45fc0683edc727331
Recoder <gogoba@users.noreply.github.com>
2022-01-19 16:19:28 +0100

    refactor: WINNT port adjustments do not use Boost Conan package.

--- configure.py
+++ configure.py
@@ -3,3 +3,5 @@ import subprocess
 import os
 subprocess.run(["conan", "install", ".", "-s", "build_type=Debug", "--install-folder=cmake-build-debug","--build=missing"])
 #subprocess.run(["conan", "install", ".", "-s", "build_type=Release", "--install-folder=cmake-build-release","--build=missing"])
+
+# Windows -G "Ninja Multi-Config" -DCONAN_DISABLE_CHECK_COMPILER=1 | set CONAN_REVISIONS_ENABLED=1 && conan install --build=missing --remote=bincrafters .
