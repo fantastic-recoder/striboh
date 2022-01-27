61e322819a33d7e4b00290f4840c4c99f1fa4e4a
Recoder <gogoba@users.noreply.github.com>
2022-01-27 18:56:50 +0100

    fix: fixed unit tests.

--- configure.py
+++ configure.py
@@ -5,9 +5,20 @@ import os
 ## git submodule init update
 # https://github.com/boostorg/boost
 if os.name == 'nt' :
-    subprocess.run(["cmake", "install", ".", "-s", "build_type=Release", "--install-folder=cmake-build-release","--build=missing"])
+    subprocess.run(["cmake", "install", ".", 
+                    "-s", "build_type=RelWithDebInfo", 
+                    "--install-folder=cmake-build-relwithdebinfo-msvc22",
+                    "--build=missing","-G", 
+                    "Visual Studio 17 2022"
+                    ])
+    subprocess.run(["cmake", "install", ".", 
+                    "-s", "build_type=RelWithDebInfo", 
+                    "--install-folder=cmake-build-relwithdebinfo-ninja",
+                    "--build=missing","-G", 
+                    "Ninja Multi-Config"
+                    ])
 else:
     subprocess.run(["conan", "install", ".", "-s", "build_type=Release", "--install-folder=cmake-build-release","--build=missing"])
+    subprocess.run(["conan", "install", ".", "-s", "build_type=Debug", "--install-folder=cmake-build-debug","--build=missing"])
 
 # Windows -G "Ninja Multi-Config" -DCONAN_DISABLE_CHECK_COMPILER=1 | set CONAN_REVISIONS_ENABLED=1 && conan install --build=missing --remote=bincrafters .
-#cmake -G "Visual Studio 17 2022"
